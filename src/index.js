307a6a2bf215f5aba349f5d1b14b98ee46ec7d45
schoningerJimmy <jimmy.schoninger32@outlook.fr>
2020-04-22 23:12:11 +0200

    add the icon and the server file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+
+
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
