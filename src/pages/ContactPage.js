d74d8451bb1dbe05e4af3f0919e434b64e29966f
schoningerJimmy <jimmy.schoninger32@outlook.fr>
2020-04-22 04:40:00 +0200

    contact page css and form added

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,19 +1,34 @@
 import React from 'react'
-import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
+import { Button, Container, Row, Form, Col } from 'react-bootstrap';
+import './ContactPage.css'
 
 function ContactPage(props) {
     return(
-        <Jumbotron className="bg-transparent jumbotron-fluid p-0">
-            <Container fluid={true}>
-                <Row className="justify-content-center py-3">
-                    <Col md={8} sm={12}>
-                        <h1 className="contact-header">Contact Me</h1>
-                        <h1>test 2</h1>
-                        
+        
+        <Container fluid={true}>
+                <Row className="justify-content-center py-3 mt-4">
+                    <Col xs={12} sm={8}>
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label style={{color: 'white'}}>Name</Form.Label>
+                                <Form.Control placeholder="Enter your name" />
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label style={{color: 'white'}}>Phone Number</Form.Label>
+                                <Form.Control placeholder="Enter Your Phone Number" />
+                            </Form.Group>
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label style={{color: 'white'}}>Information Request</Form.Label>
+                                <Form.Control as="textarea" rows="3" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Send
+                            </Button>
+                        </Form>
                     </Col>
                 </Row>
             </Container>
-        </Jumbotron>
     );
 
 }
